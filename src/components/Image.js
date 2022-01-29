a3b8ebb97152a6c3fe5cbd12b2007fa6ddabaddf
Ben Goshow <ben.goshow@gmail.com>
2022-01-29 10:04:55 -0500

    favicon

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,7 @@ function Image(props) {
   const srcExists = props.src,
     imgHeight = props.height,
     imgWidth = props.width;
-  if (srcExists != 'spoiler' && imgHeight && imgWidth) {
+  if (srcExists !== 'spoiler' && imgHeight && imgWidth) {
     return <img src={props.src} alt={props.alt} />;
   }
   return <img src="http://via.placeholder.com/640x360?text=Sneaky+Image" alt="" />;
